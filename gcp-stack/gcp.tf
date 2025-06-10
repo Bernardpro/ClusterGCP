ab0591806521e889e8c3501d3fa73c809c247803
bernardpro <ber.maxime.paul@gmail.com>
2025-06-10 22:39:27 +0200

    delete folder from lucas

--- gcp-stack/gcp.tf
+++ gcp-stack/gcp.tf
@@ -54,7 +54,7 @@ resource "google_container_node_pool" "nodes" {
 
   autoscaling {
     min_node_count = 1
-    max_node_count = 4
+    max_node_count = 2
   }
 
 
--- longhorn/setup.sh
+++
@@ -1,8 +0,0 @@
-curl -sSfL https://raw.githubusercontent.com/longhorn/longhorn/v1.7.2/scripts/environment_check.sh | bash
-sudo apt-get install open-iscsi
-sudo modprobe iscsi_tcp
-sudo systemctl enable iscsid
-sudo systemctl start iscsid
-sudo apt-get install nfs-common
-sudo systemctl stop multipathd multipathd.socket
-sudo systemctl disable multipathd multipathd.socket
